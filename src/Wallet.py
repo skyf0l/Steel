493180c3d19cc16b6b330c70ab5857957e8c5441
Skyf0l <tom.rorato@epitech.eu>
2020-12-16 09:41:05 +0100

    Add private and public key prototypes

--- src/Wallet.py
+++ src/Wallet.py
@@ -1,4 +1,21 @@
+import ecdsa
+from hashlib import sha256
+
 class Wallet:
 
 	def __init__(self, name):
 		self.address = name
+
+		sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1, hashfunc=sha256)
+
+		self.privatekey = sk
+		self.publickey = sk.get_verifying_key()
+
+		'''
+		print(sk.to_string().hex())
+		vk = sk.get_verifying_key()
+		print(vk.to_string().hex())
+		sig = sk.sign(b"message")
+		print(sig.hex())
+		print(vk.verify(sig, b"message"))
+		exit()'''
